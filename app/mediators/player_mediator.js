21eabe366de6c4c0c56079bb63362dd4b7debab1
Janj <grasmaaier45@gmail.com>
2015-04-30 17:55:43 +0200

    added basic physics (in the wrong places though, needs refactoring) and animations (ditto, needs to be abstracted from player)

--- app/mediators/player_mediator.js
+++ app/mediators/player_mediator.js
@@ -2,22 +2,41 @@
 
     'use strict';
 
-    berghain2.PlayerMediator = function(target, dispatcher, mediators, lo, input) {
+    berghain2.PlayerMediator = function(target, game, dispatcher, mediators, lo, input) {
 
         lo.g("MEDIATOR", "Player mediator instantiated", target);
 
+
+        // THIS HAS TO HAPPEN SOMEWHERE ELSE
+         game.physics.arcade.gravity.y = 2500;
+		game.physics.enable(target, Phaser.Physics.ARCADE);     
+
+		target.body.collideWorldBounds = true;   
+
+        target.animations.add('left', [30, 31, 32, 33, 34, 35, 36, 37], 10, true);
+        target.animations.add('right', [10, 11, 12, 13, 14, 15, 16, 17], 10, true)
+
+        // TODO add collisions with floor
+
+
         dispatcher.addEventListener('game_update', function(event) {
+
             if (input.goLeft) {
-                lo.g("USER", "LEFT");
+                target.animations.play('left');
+                target.position.x -= 2;
             }
             if (input.goUp) {
-                lo.g("USER", "UP ");
+                target.body.velocity.y = -300;
             }
             if (input.goRight) {
-                lo.g("USER", "RIGHT ");
+                target.animations.play('right');
+                target.position.x += 2;
             }
-            if (input.goDown) {
-                lo.g("USER", "DOWN ");
+            if (input.goDown) {}
+
+            if (!input.goLeft && !input.goRight) {
+                target.animations.stop();
+                target.frame = 2;
             }
         });
 
