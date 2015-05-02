0e6187d2e13ee9643f27af619da17e78c6807f23
WWWillems <lander.willems@gmail.com>
2015-05-02 23:36:20 +0200

    Housekeeping. Added some items to TODO.md

--- TODO.md
+++ TODO.md
@@ -1,6 +1,11 @@
 ## TODO's
 
- - Create pause screen
+ - Create pause screens (Check out Phaser's system first)
  - Every object needs a .destroy() function
  - Using Tilemaps for level design -  [Download](http://www.mapeditor.org/download.html) and  [Tutorial](http://gamedevelopment.tutsplus.com/tutorials/introduction-to-tiled-map-editor--gamedev-2838)
- - Parallax background
+ - Parallax background (needs layer overlapping player as well)
+ - Think of a system to draw animated pixel art 'light flicker'. Draw some circles bro.
+ - Cheat/debug shortcuts
+ - Player damage needs to move from firebin to command
+ - Add raycastings (http://gamemechanicexplorer.com/#raycasting-1)
+ 
--- app/commands/create_world_command.js
+++ app/commands/create_world_command.js
@@ -1,13 +1,12 @@
-(function(berghain2) {
+(function (berghain2) {
 
     'use strict';
 
-    berghain2.CreateWorldCommand = function(dispatcher, mediators, lo, config, game, input, physics_model) {
+    berghain2.CreateWorldCommand = function (dispatcher, mediators, lo, config, game, input, physics_model) {
 
-        this.execute = function(event) {
+        this.execute = function (event) {
 
             lo.g("COMMAND", "Creating World");
-
 
             initGamePhysics();
             createBackground();
@@ -20,23 +19,17 @@
             createSky();
             createEnemies();
 
-                        createPlayer();
-
+            createPlayer();
 
             dispatcher.dispatch('create_hud');
         }
 
         function initGamePhysics() {
-
             // Define world bounds
             game.world.setBounds(0, 0, game.width, 3000);
 
             //  We're going to be using physics, so enable the Arcade Physics system
             game.physics.startSystem(Phaser.Physics.ARCADE);
-            //game.stage.smoothed = false;
-
-
-
         }
 
         function createEnemies() {
