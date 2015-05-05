1e061b32a088d96d3f76d591e64e7098eaefdfe5
Mathieu <mathieu.allaert@opus-magnum.com>
2015-05-06 01:25:49 +0200

    Resolves #1

--- app/mediators/camera_mediator.js
+++ app/mediators/camera_mediator.js
@@ -4,9 +4,8 @@
     berghain2.CameraMediator = function (target,  camera_model, lo, dispatcher, game) {
         lo.g("MEDIATOR", "Camera mediator instantiated", target);
         var self = this;
-        this.tween = game.add.tween(game.camera).to( { x:0, y:0}, 1000, "Sine.easeInOut", false, -1, false);
-        this.bufferWidth = game.width/6.0;
-
+        this.bufferWidth = game.width/3.0;
+        this.smoothCamera = 7.0; //the lower, the smoother
         dispatcher.addEventListener('game_update', function (event) {
             if(typeof(camera_model.cameraTarget != 'undefined'))
             {
@@ -25,9 +24,8 @@
 
                 if(xOffset != game.camera.x)
                 {
-                    var speedMul = 10.0;
                     //smooth offset
-                    var elapsedTime = Math.min(game.time.elapsedMS/1000.0*speedMul,1);
+                    var elapsedTime = Math.min(game.time.elapsedMS/1000.0*self.smoothCamera,1);
                     var diffX = xOffset*elapsedTime;
                     game.camera.x += diffX;
                 }
