f27bcd37a222970fdf2ab0dbfa333b20ac4755cc
Janj <grasmaaier45@gmail.com>
2015-05-08 18:11:34 +0200

    added getter and setter patern in PlayerModel

--- app/models/player_model.js
+++ app/models/player_model.js
@@ -1,18 +1,30 @@
 (function(berghain2) {
 
-	'use strict';
+    'use strict';
 
-	var PlayerModel = function(dispatcher,input,lo,config,game) {
-		lo.g("MODEL","Player Model Initiated");
-		 
-		 this.xPosition = 0;
-		 this.yPosition = 0;
-		 
-		 this.health = 100;
-		 this.mana = 5;
+    var PlayerModel = function(dispatcher, input, lo, config, game) {
+        lo.g("MODEL", "Player Model Initiated");
 
-	};
-	
-	berghain2.PlayerModel = PlayerModel;
+        this.xPosition = 0;
+        this.yPosition = 0;
+
+        this.health = 100;
+
+
+        this.mana = 5;
+
+    };
+
+    PlayerModel.prototype = {
+        get health() {
+            return this._health;
+        },
+        set health(val) {
+        	// add check for health
+            this._health = val;
+        }
+    };
+
+    berghain2.PlayerModel = PlayerModel;
 
 })(window.berghain2 = window.berghain2 || {});
